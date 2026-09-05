18e857f9376cdd8f2e413f884f0d9101e22e1e69
aimbxt <067alexl@gmail.com>
2026-09-04 22:03:20 -0500

    connect to mongodb database

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
+import "dotenv/config";
 import app from './app.ts'
+import { connectToDatabase } from "./config/database.ts";
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+async function startServer() {
+    await connectToDatabase();
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    })
+}
+
+startServer();

96ebd89c2fa44d9cd8e09a46f7a99b5b3e12c934
aimbxt <067alexl@gmail.com>
2026-09-13 22:39:58 -0500

    implement shift routes

--- src/routes/shiftRoutes.ts
+++ src/routes/shiftRoutes.ts
@@ -1,6 +1,11 @@
-import express from 'express';
+import { Router } from "express";
 import * as shiftController from '../controllers/shiftController.ts';
 
-const router = express.Router();
-router.post('/');
+const router = Router();
+router.get("/", shiftController.getShifts);
+router.get("/:id", shiftController.getShiftById);
+router.post("/", shiftController.createShift);
+router.patch("/:id", shiftController.updateShift);
+router.delete("/:id", shiftController.deleteShift);
+
 export default router;

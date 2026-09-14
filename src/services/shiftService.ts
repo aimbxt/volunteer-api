02e456b880e0224d222f1ae7ac1fdf2a0047b283
aimbxt <067alexl@gmail.com>
2026-09-13 21:51:41 -0500

    finish shiftservice basic functions

---
+++ src/services/shiftService.ts
@@ -0,0 +1,28 @@
+import { Shift } from "../models/Shift.ts";
+
+type CreateShiftInput = { title: string; description?: string; location: string; startTime: Date; endTime: Date; capacity: number; }
+export async function createShift(data: CreateShiftInput) {
+    return await Shift.create(data);
+}
+
+export async function getShifts() {
+    return await Shift.find();
+}
+
+export async function getShiftById(id: string) {
+    return await Shift.findById(id);
+}
+
+export async function updateShift(id: string, data: Partial<{ title: string; description: string; location: string; startTime: Date; endTime: Date; capacity: number }>) {
+    const shift = await getShiftById(id);
+    if (shift) {
+        Object.assign(shift, data);
+        await shift.save();
+    }
+    return shift;
+}
+
+export async function deleteShift(id: string) {
+    const deleted = await Shift.findByIdAndDelete(id);
+    return deleted;
+}
